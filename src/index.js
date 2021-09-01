6a33ab26a44dd1d0040cd06ff8cc1fe549f798df
wasifdawar <m.wasifdawar@gmail.com>
2021-09-02 03:46:02 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import spiner from './spiner';
+import Spiner from './spiner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+        
+        
+    }
+
+    componentDidMount()
+    {
+             window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }),
+                 (err) => this.setState({ errorMessage: err.message })
+             
+             );
+    }
+
+
+    renderContent()
+    {
+        if (this.state.errorMessage && !this.state.lat)
+        { return <div>Error:{ this.state.errorMessage}</div>    }
+
+        if (!this.state.errorMessage && this.state.lat)
+        { return <SeasonDisplay lat={this.state.lat}/>}
+  
+        return <spiner/>
+    }
+    
+    render() {
+        return <div className="border-red">
+          {this.renderContent()} 
+       </div>
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/spiner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const spiner = () =>
+{
+    return 
+    <div class="ui active dimmer">
+      <div className="ui big text loader">Loading...</div>
+     </div>
+}
+    
+
+export default spiner;
